e71a616f4e927b3a4f02d2bf37b68ff7c0ad57f8
namra-38 <nimra.rahim99@gmail.com>
2021-06-03 13:02:38 +0500

    Account model created

--- notes_taking_app/accounts/models.py
+++ notes_taking_app/accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Account(AbstractUser):
+  GENDER_CHOICES = (
+    ('male', 'Male'),
+    ('female', 'Female'),
+  )
+  default_img_path = "media/utility_images/default.png"
+  uploaded_img_path = "media/profile_images/"
+
+  # model fields
+  profile_img = models.ImageField(upload_to=uploaded_img_path, default=default_img_path)
+  gender = models.CharField(max_length=7, choices=GENDER_CHOICES)
+
+  def __str__(self):
+    return self.username
+
+
