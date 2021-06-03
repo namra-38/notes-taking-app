0442130334b4e6967ae98ab8b376f62e5954da1a
namra-38 <nimra.rahim99@gmail.com>
2021-06-03 13:03:08 +0500

    added url patterns for app

--- notes_taking_app/accounts/urls.py
+++ notes_taking_app/accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-  
+  path('login/', views.login_view, name='login'),
+  path('signup/', views.signup_view, name='signup'),
+  path('check_validity/', views.check_username_validity, name='check_validity'),
 ]
