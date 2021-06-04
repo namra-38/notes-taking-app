f7d21bd198deb71a5bd22664581ce1c797767366
namra-38 <nimra.rahim99@gmail.com>
2021-06-04 11:53:32 +0500

    Patterns for logout, home and password change added

--- notes_taking_app/accounts/urls.py
+++ notes_taking_app/accounts/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
   path('login/', views.login_view, name='login'),
+  path('logout/', views.logout_view, name='logout'),
   path('signup/', views.signup_view, name='signup'),
+  path('home/', views.home_view, name='home_page'),
+  path('change_password/', views.password_change_view, name='password_change'),
   path('check_validity/', views.check_username_validity, name='check_validity'),
 ]
