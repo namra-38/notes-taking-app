139a70a5a64a66cd82c6234ba4bcecdf7dc7d748
namra-38 <nimra.rahim99@gmail.com>
2021-06-08 12:58:37 +0500

    profile update view pattern added

--- notes_taking_app/accounts/urls.py
+++ notes_taking_app/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'accounts'
@@ -6,7 +7,27 @@ urlpatterns = [
   path('login/', views.login_view, name='login'),
   path('logout/', views.logout_view, name='logout'),
   path('signup/', views.signup_view, name='signup'),
+  path('update-profile/', views.profile_update_view, name='update_profile'),
   path('home/', views.home_view, name='home_page'),
-  path('change_password/', views.password_change_view, name='password_change'),
-  path('check_validity/', views.check_username_validity, name='check_validity'),
+  path('change-password/', views.password_change_view, name='password_change'),
+  path('check-validity/', views.check_username_validity, name='check_validity'),
+  path('password-reset/', views.password_reset_view, name='password_reset'),
+]
+
+# BUILT-IN PASSWORD RESET PATTERNS
+urlpatterns += [
+
+  path('reset/<uidb64>/<token>/',views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'
+  ),
+
+  path('reset-password-sent/', 
+    auth_views.PasswordResetDoneView.as_view(
+      template_name='accounts/authentications/password_reset_sent.html'
+    ), name='password_reset_done'
+  ),
+
+  path('reset-password-complete/',
+    auth_views.PasswordResetCompleteView.as_view(
+      template_name='accounts/authentications/password_reset_complete.html'), name='password_reset_complete'
+  ),
 ]
