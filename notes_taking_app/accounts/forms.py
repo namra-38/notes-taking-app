54fea3b962aa90f008bdb02ae1b3eaa19fd726ab
namra-38 <nimra.rahim99@gmail.com>
2021-06-08 12:55:18 +0500

    Profile Update Form added

--- notes_taking_app/accounts/forms.py
+++ notes_taking_app/accounts/forms.py
@@ -58,7 +58,47 @@ class SignUpForm(forms.ModelForm):
 
     if password != re_password:
       raise forms.ValidationError("Password didn't Matched")
+    return re_password 
+
+#  * SOME REDUNDANT CODE DOWN THERE, NEEDS TO BE OPTIMIZE 
+
+class ProfileUpdateForm(forms.ModelForm):
+  def __init__(self, *args, **kwargs):
+    super(ProfileUpdateForm, self).__init__(*args, **kwargs)
+    for field_name in self.fields.keys():
+      self.fields[field_name].widget.attrs.update(
+        {'class': 'form-control'}
+      )
+
+  class Meta:
+    model = Account
+    fields = ['username', 'first_name', 'last_name', 'email', 'profile_img', 'gender'] 
+    
+  def clean_email(self):
+    email = self.cleaned_data.get('email')  
+    valid_email_extentions = ('.com', '.org', '.net')
+    
+    if "@" not in email or not email.endswith(valid_email_extentions):
+      raise forms.ValidationError('Incorrect Email Format')
+    return email 
+
+  def clean_re_password(self):
+    password = self.cleaned_data.get('password') 
+    re_password = self.cleaned_data.get('re_password') 
+
+    if password != re_password:
+      raise forms.ValidationError("Password didn't Matched")
     return re_password  
+    
+  def clean_profile_img(self):
+    image_object = self.cleaned_data.get('profile_img')
+    image = str(image_object) 
+    image_valid_extension = ('.png', '.jpg', '.jpeg')
+
+    if not image.endswith(image_valid_extension):
+      raise forms.ValidationError('Invalid image type')
+    return image_object  
+
 
 
 
