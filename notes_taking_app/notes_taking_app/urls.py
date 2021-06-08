e68e432e359c842d6caf67739e18568dc36a4fbc
namra-38 <nimra.rahim99@gmail.com>
2021-06-08 12:59:56 +0500

    aull-auth urlpatterns added

--- notes_taking_app/notes_taking_app/urls.py
+++ notes_taking_app/notes_taking_app/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('accounts/', include('allauth.urls')),
     path('notes/', include('notes.urls', namespace='notes')),
 ]
 if settings.DEBUG:
